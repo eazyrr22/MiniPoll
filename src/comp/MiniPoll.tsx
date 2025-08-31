6b17ec99314fd376eee1d471fbe56965fb295225
eazy <ezrirot@gmail.com>
2025-08-31 12:33:44 +0300

    Add state for options, votes, showResults, and input

--- src/comp/MiniPoll.tsx
+++ src/comp/MiniPoll.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function MiniPoll() {
+    const [options,setOptions] = useState('default')
+    const [votes,setVotes] = useState(0)
+    const [result,setResult] = useState(false)
+    const [input,setInput] = useState('')
   return (
     <h2>Mini Poll</h2>
   )
