396baf98103def1137655e4877ad8b7f7e7577a7
eazy <ezrirot@gmail.com>
2025-08-31 14:41:30 +0300

    fixed assingment

--- src/comp/MiniPoll.tsx
+++ src/comp/MiniPoll.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { useState } from 'react'
 
 export default function MiniPoll() {
-    const [options,setOptions] = useState('default')
-    const [votes,setVotes] = useState(0)
-    const [result,setResult] = useState(false)
+    const [options,setOptions] = useState([])
+    const [votes,setVotes] = useState([])
+    const [showResult,setshowResult] = useState(false)
     const [input,setInput] = useState('')
   return (
     <h2>Mini Poll</h2>
